374224cf9817bfa0fc12d3dda399d8c74724e2e7
Bintang Ramadhan <mbintangr13@gmail.com>
2023-06-24 11:29:18 +0700

    change products button to recipes button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import Button from './Button';
 const Navbar = () => {
   return (
     <div className='text-center flex p-7 justify-between bg-[#001C30] align-middle text-[#DAFFFB] items-center w-[100vw] px-[15vw] fixed shadow-xl'>
-        <h1 className='text-3xl font-bold'>CTLG</h1>
+        <h1 className='text-3xl font-bold'>Cravery</h1>
         <div className='flex items-center'>
             <ul className='flex'>
                 <li className='mx-3'><a href="#">Home</a></li>
                 <li className='mx-3'><a href="/About">About</a></li>
             </ul>
-            <Button link="/Product" title="Products" />
+            <Button link="/Recipes" title="Recipes" />
         </div>
     </div>
   )
