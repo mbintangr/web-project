3c4822378b0cbfbb2feff40362eab3649cfd1244
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-04 10:05:00 +0700

    add icon to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo192.png";
 
 const Navbar = () => {
   return (
-    <div className='text-center flex p-7 justify-between bg-primary align-middle text-quaternary items-center w-[100vw] px-[15vw] fixed shadow-xl'>
-        <h1 className='text-3xl font-bold'><Link to="/">Cravery</Link></h1>
-        <div className='flex items-center'>
-            <ul className='flex'>
+    <div className="text-center flex p-7 justify-between bg-primary align-middle text-quaternary items-center w-[100vw] px-[15vw] fixed shadow-xl">
+      <Link to="/">
 
-                <li className='mx-3'>
-                  <Link to="/About">About</Link>  
-                </li>
-
-                <li className='mx-3'>
-                  <Link to="/Contact">Contact</Link>
-                </li>
-
-            </ul>
-            <Button link="/Recipes" title="Recipes" />
+        <div className="flex items-center">
+          <img src={logo} alt="" className="w-[50px] mr-2" />
+          <h1 className="text-3xl font-bold">
+            Cravery
+          </h1>
         </div>
+      </Link>
+      <div className="flex items-center">
+        <ul className="flex">
+          <li className="mx-3">
+            <Link to="/About">About</Link>
+          </li>
+
+          <li className="mx-3">
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+        <Button link="/Recipes" title="Recipes" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
