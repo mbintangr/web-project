7ba9a555e93a94d81fd4083b45d200a91951bf3d
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-06 09:35:44 +0700

    change logo sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="text-center sm:flex p-7 sm:justify-between bg-primary sm:align-middle text-quaternary sm:items-center w-[100vw] px-[15vw] fixed shadow-xl">
       <Link to="/">
         <div className="flex items-center">
-          <img src={logo} alt="" className="w-[50px] mr-2" />
+          <img src={logo} alt="" className="h-[50px] mr-2" />
           <h1 className="text-3xl font-bold">
             Cravery
           </h1>
