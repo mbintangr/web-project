ed81a6cabaa81ebd771c09e27845e50239b33fd8
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-06 09:36:17 +0700

    change trending layout and apply map

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,16 +1,19 @@
 import React from 'react'
 // import { Carousel } from 'flowbite-react'
 import Card from './Card'
+import { data } from '../data/data'
 
 function Trending() {
+  console.log(data);
   return (
-    <div className='bg-tertiary p-10 md:p-7 sm:p-3 mx-auto my-10 rounded-3xl shadow-xl text-primary max-w-[75vw] flex-col justify-center' id='trending'>
+    <div className='bg-tertiary p-7 mx-auto my-10 rounded-3xl shadow-xl text-primary max-w-[75vw] flex-col justify-center' id='trending'>
         <h1 className='text-3xl font-bold pb-5 text-center'>Trending🔥</h1>
 
-        <div className='flex mx-auto flex-wrap'>
-            <Card src="https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1200x900.jpg" alt="Rendang" title="Rendang" description="Rendang is a traditional Indonesian dish that is widely recognized and celebrated for its rich and complex flavors." link="/Recipes"/>
-            <Card src="https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1200x900.jpg" alt="Rendang" title="Rendang" description="Rendang is a traditional Indonesian dish that is widely recognized and celebrated for its rich and complex flavors." link="/Recipes"/>
-            <Card src="https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1200x900.jpg" alt="Rendang" title="Rendang" description="Rendang is a traditional Indonesian dish that is widely recognized and celebrated for its rich and complex flavors." link="/Recipes"/>
+        <div className='mx-auto grid md:grid-cols-3 gap-6'>
+
+            {data.map((item, index) => (
+              <Card src={item.img} title={item.name} description={item.description} link={item.link}/>
+            ))}
 
         </div>
         
