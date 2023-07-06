5a769957afbab7dbd7252b1c47057d484f682623
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-06 09:35:13 +0700

    edit card layout and hover animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,9 @@ import Button from './Button'
 
 function Card(props) {
   return (
-    <div className='flex-col my-3 mx-auto rounded-2xl shadow-xl bg-white w-[30%] min-w-[150px]'>
-        <img src={props.src} alt={props.alt} className='rounded-t-xl shadow-lg w-[100%]' />
+    <div className='flex-col my-3 mx-auto rounded-2xl shadow-xl bg-white min-w-[30%]  hover:scale-105 duration-300'>
+        <img src={props.src} alt={props.alt} className='rounded-t-xl shadow-lg w-[100%] h-[150px] object-cover lg:h-[250px]'/>
+
         <div className='p-7'>
           <h1 className='text-xl font-bold mt-2'>{props.title}</h1>
           <p className='my-2'>{props.description}</p>
