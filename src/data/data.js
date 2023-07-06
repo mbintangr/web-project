d03efe149824702f97542eb84eb940f32cd2540f
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-06 09:38:02 +0700

    insert trending data

---
+++ src/data/data.js
@@ -0,0 +1,65 @@
+export const trending = [
+    {
+        id: 1,
+        name: 'Rendang',
+        description: 'Rendang is a traditional Indonesian dish known for its tender, slow-cooked meat in a rich coconut curry. It is made with beef or chicken simmered with spices and coconut milk, resulting in a flavorful and aromatic dish.',
+        img: 'https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1200x900.jpg',
+        link: '/Recipes'
+    },
+    {
+        id: 2,
+        name: 'Sushi',
+        description: 'Sushi is a popular Japanese dish consisting of vinegared rice accompanied by various ingredients, such as raw or cooked seafood, vegetables, and sometimes tropical fruits.',
+        img: 'https://images.pexels.com/photos/8518854/pexels-photo-8518854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 3,
+        name: 'Pasta Carbonara',
+        description: 'Pasta Carbonara is a classic Italian dish made with spaghetti, eggs, pancetta or bacon, grated cheese, and black pepper. It is known for its creamy and savory flavor.',
+        img: 'https://images.pexels.com/photos/1438672/pexels-photo-1438672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 4,
+        name: 'Tacos',
+        description: 'Tacos are a traditional Mexican dish consisting of tortillas filled with various ingredients, such as seasoned meat, cheese, salsa, guacamole, and lettuce.',
+        img: 'https://images.pexels.com/photos/4958773/pexels-photo-4958773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 5,
+        name: 'Pad Thai',
+        description: 'Pad Thai is a popular Thai stir-fried noodle dish made with rice noodles, eggs, tofu, shrimp or chicken, bean sprouts, peanuts, and lime.',
+        img: 'https://images.pexels.com/photos/12561885/pexels-photo-12561885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 6,
+        name: 'Pizza Margherita',
+        description: 'Pizza Margherita is a classic Italian pizza topped with fresh tomatoes, mozzarella cheese, basil leaves, and olive oil. It is known for its simplicity and delicious flavors.',
+        img: 'https://images.pexels.com/photos/6605214/pexels-photo-6605214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 7,
+        name: 'Hamburger',
+        description: 'Hamburger is an American staple consisting of a ground beef patty, typically served in a bun with various toppings like lettuce, tomato, onions, pickles, and condiments.',
+        img: 'https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 8,
+        name: 'Sushi Rolls',
+        description: 'Sushi Rolls, also known as makizushi, are cylindrical sushi rolls filled with a variety of ingredients, such as fish, vegetables, and rice, wrapped in nori seaweed.',
+        img: 'https://images.pexels.com/photos/3642030/pexels-photo-3642030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    },
+    {
+        id: 9,
+        name: 'Chicken Tikka Masala',
+        description: 'Chicken Tikka Masala is a popular Indian dish consisting of grilled chicken pieces cooked in a spiced tomato-based sauce. It is best enjoyed with naan bread or rice.',
+        img: 'https://images.pexels.com/photos/5946612/pexels-photo-5946612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        link: '/Recipes'
+    }
+];
