5005c3df68c30651b5fb5cc45d789f43aa3f85a6
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-07 10:56:12 +0700

    recipes page first design

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {recipes} from '../data/data' 
+import Card from '../components/Card'
 
 const Recipes = (props) => {
   return (
     <div className='px-[15vw] pt-[130px]'>
-      <h1 className='text-3xl font-bold'>{props.foodName} Recipes!</h1>
+      <h1 className='text-3xl font-bold mb-5'>{props.foodName} Recipes!</h1>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10'>
+        {recipes.map((recipe, index) => (
+          <Card src={recipe.img} title={recipe.name} description={recipe.description} link={recipe.link}/>
+        ))}
+      </div>
     </div>
   )
 }
