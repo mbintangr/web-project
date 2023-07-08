45e7bf8c2fcf678054c9a3563cfa3c3ba07b28da
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-08 15:53:48 +0700

    formatter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Welcome from '../components/Welcome'
-import Information from '../components/Information'
-import Trending from '../components/Trending'
+import React from "react";
+import Welcome from "../components/Welcome";
+import Information from "../components/Information";
+import Trending from "../components/Trending";
 
 const Home = () => {
   return (
-    <div className='flex-col justify-center'>
-        <Welcome />
-        <Information />
-        <Trending />
+    <div className="flex-col justify-center">
+      <Welcome />
+      <Information />
+      <Trending />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
