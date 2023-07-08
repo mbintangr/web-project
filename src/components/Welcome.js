04de9aead1c93efe1b6dfc840f85ec642916dc52
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-08 16:00:41 +0700

    remove unused comment

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,9 +14,3 @@ const Welcome = () => {
 };
 
 export default Welcome;
-
-{/* <div className='flex flex-col justify-center h-[100vh] px-[15vw]'>
-      <h1 className='text-3xl font-bold py-2'>Hello There!👋</h1>
-      <p className='pt-2 pb-4'>Welcome to Cravey, your ultimate destination for delicious treats and comforting comfort food. Our curated collection of recipes will satisfy your cravings and bring joy to your taste buds. With expert chefs, detailed instructions, and a vibrant community of food lovers, Cravey is where culinary dreams come true. Embark on a mouthwatering journey and indulge in the most satisfying flavors at Cravey.</p>
-      <a href="#trending"><button className='bg-secondary py-2 px-3 rounded-2xl font-bold text-quaternary shadow-lg hover:bg-[#176b87c9]'>Trending</button></a>
-    </div> */}
