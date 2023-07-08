0b7e9697055dbb4b4b2271d18aedf22df145d83e
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-08 15:59:03 +0700

    add new recipe's attributes

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,4 @@
 export const recipes = [
-  // Existing data
   {
     id: 1,
     name: "Rendang",
@@ -7,6 +6,8 @@ export const recipes = [
       "Rendang is a traditional Indonesian dish known for its tender, slow-cooked meat in a rich coconut curry. It is made with beef or chicken simmered with spices and coconut milk, resulting in a flavorful and aromatic dish.",
     img: "https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1200x900.jpg",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 2,
@@ -15,6 +16,8 @@ export const recipes = [
       "Sushi is a popular Japanese dish consisting of vinegared rice accompanied by various ingredients, such as raw or cooked seafood, vegetables, and sometimes tropical fruits.",
     img: "https://images.pexels.com/photos/8518854/pexels-photo-8518854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 3,
@@ -23,6 +26,8 @@ export const recipes = [
       "Pasta Carbonara is a classic Italian dish made with spaghetti, eggs, pancetta or bacon, grated cheese, and black pepper. It is known for its creamy and savory flavor.",
     img: "https://images.pexels.com/photos/1438672/pexels-photo-1438672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Easy",
   },
   {
     id: 4,
@@ -31,6 +36,8 @@ export const recipes = [
       "Tacos are a traditional Mexican dish consisting of tortillas filled with various ingredients, such as seasoned meat, cheese, salsa, guacamole, and lettuce.",
     img: "https://images.pexels.com/photos/4958773/pexels-photo-4958773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "America",
+    difficulty: "Easy",
   },
   {
     id: 5,
@@ -39,6 +46,8 @@ export const recipes = [
       "Pad Thai is a popular Thai stir-fried noodle dish made with rice noodles, eggs, tofu, shrimp or chicken, bean sprouts, peanuts, and lime.",
     img: "https://images.pexels.com/photos/12561885/pexels-photo-12561885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 6,
@@ -47,6 +56,8 @@ export const recipes = [
       "Pizza Margherita is a classic Italian pizza topped with fresh tomatoes, mozzarella cheese, basil leaves, and olive oil. It is known for its simplicity and delicious flavors.",
     img: "https://images.pexels.com/photos/6605214/pexels-photo-6605214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Easy",
   },
   {
     id: 7,
@@ -55,6 +66,8 @@ export const recipes = [
       "Hamburger is an American staple consisting of a ground beef patty, typically served in a bun with various toppings like lettuce, tomato, onions, pickles, and condiments.",
     img: "https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "America",
+    difficulty: "Easy",
   },
   {
     id: 8,
@@ -63,6 +76,8 @@ export const recipes = [
       "Sushi Rolls, also known as makizushi, are cylindrical sushi rolls filled with a variety of ingredients, such as fish, vegetables, and rice, wrapped in nori seaweed.",
     img: "https://images.pexels.com/photos/3642030/pexels-photo-3642030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 9,
@@ -71,8 +86,9 @@ export const recipes = [
       "Chicken Tikka Masala is a popular Indian dish consisting of grilled chicken pieces cooked in a spiced tomato-based sauce. It is best enjoyed with naan bread or rice.",
     img: "https://images.pexels.com/photos/5946612/pexels-photo-5946612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
-  // Additional data
   {
     id: 10,
     name: "Biryani",
@@ -80,6 +96,8 @@ export const recipes = [
       "Biryani is a flavorful South Asian rice dish made with basmati rice, meat (such as chicken, lamb, or fish), and a blend of aromatic spices. It is often garnished with fried onions, boiled eggs, and served with raita.",
     img: "https://images.pexels.com/photos/12737656/pexels-photo-12737656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Hard",
   },
   {
     id: 11,
@@ -88,6 +106,8 @@ export const recipes = [
       "Pho is a Vietnamese soup consisting of broth, rice noodles, and thinly sliced meat (such as beef or chicken). It is commonly garnished with fresh herbs, bean sprouts, lime, and chili peppers.",
     img: "https://images.pexels.com/photos/6646072/pexels-photo-6646072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 12,
@@ -96,6 +116,8 @@ export const recipes = [
       "Lasagna is a classic Italian dish made with layers of flat pasta sheets, meat sauce (usually containing ground beef or sausage), cheese (such as mozzarella and ricotta), and often topped with a tomato sauce.",
     img: "https://images.pexels.com/photos/4079520/pexels-photo-4079520.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 13,
@@ -104,6 +126,8 @@ export const recipes = [
       "Sushi Bowl, also known as Poke Bowl, is a deconstructed version of sushi served in a bowl. It typically includes sushi rice topped with cubed raw fish (such as tuna or salmon), vegetables, and various toppings.",
     img: "https://images.pexels.com/photos/5697277/pexels-photo-5697277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 14,
@@ -112,6 +136,8 @@ export const recipes = [
       "Tiramisu is a popular Italian dessert made with layers of ladyfingers soaked in coffee and rum, layered with a creamy mixture of mascarpone cheese, eggs, and sugar. It is often dusted with cocoa powder and served chilled.",
     img: "https://images.pexels.com/photos/6880219/pexels-photo-6880219.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 15,
@@ -120,6 +146,8 @@ export const recipes = [
       "Steak is a tender and juicy cut of beef that is typically grilled or pan-seared to perfection. It is often seasoned with salt, pepper, and various herbs or marinades, and can be served with side dishes like mashed potatoes and vegetables.",
     img: "https://images.pexels.com/photos/3535383/pexels-photo-3535383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "America",
+    difficulty: "Medium",
   },
   {
     id: 16,
@@ -128,6 +156,8 @@ export const recipes = [
       "Chicken Curry is a flavorful dish made with chicken pieces cooked in a spicy curry sauce, typically made with a blend of spices, onions, tomatoes, ginger, and garlic. It is often served with rice or bread.",
     img: "https://images.pexels.com/photos/7353379/pexels-photo-7353379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 17,
@@ -136,6 +166,8 @@ export const recipes = [
       "Ceviche is a seafood dish popular in Latin American cuisine, particularly in Peru. It is made by marinating raw fish or seafood in citrus juice (such as lime or lemon), along with onions, peppers, and various seasonings.",
     img: "https://images.pexels.com/photos/15352336/pexels-photo-15352336/free-photo-of-ceviche.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "South America",
+    difficulty: "Easy",
   },
   {
     id: 18,
@@ -144,6 +176,8 @@ export const recipes = [
       "Miso Soup is a traditional Japanese soup made with miso paste, tofu, seaweed, and various ingredients like mushrooms, green onions, and fish or vegetable broth. It is often served as a starter in Japanese meals.",
     img: "https://images.pexels.com/photos/8982387/pexels-photo-8982387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 19,
@@ -152,6 +186,8 @@ export const recipes = [
       "Chicken Satay is a popular Southeast Asian dish made with skewered and grilled chicken marinated in a flavorful blend of spices and served with a peanut sauce for dipping. It is often accompanied by cucumber and rice or bread.",
     img: "https://images.pexels.com/photos/9056984/pexels-photo-9056984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 20,
@@ -160,6 +196,8 @@ export const recipes = [
       "Gnocchi are soft dumplings made from potatoes, flour, and sometimes eggs. They are typically boiled and served with various sauces, such as tomato sauce, pesto, or cheese sauce. Gnocchi can be a delicious alternative to pasta.",
     img: "https://images.pexels.com/photos/6659622/pexels-photo-6659622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 21,
@@ -168,6 +206,8 @@ export const recipes = [
       "Falafel is a Middle Eastern dish made from ground chickpeas or fava beans, mixed with herbs and spices, and deep-fried into crispy balls or patties. It is often served in pita bread with tahini sauce, salad, and pickles.",
     img: "https://images.pexels.com/photos/8286779/pexels-photo-8286779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 22,
@@ -176,6 +216,8 @@ export const recipes = [
       "Goulash is a Hungarian stew made with tender chunks of meat (usually beef or pork) and seasoned with paprika, onions, and other spices. It is often served with bread or noodles.",
     img: "https://images.pexels.com/photos/5713756/pexels-photo-5713756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 23,
@@ -184,6 +226,8 @@ export const recipes = [
       "Moussaka is a layered casserole dish popular in Greek cuisine. It typically consists of eggplant, ground meat (often lamb), and a creamy béchamel sauce. It is baked to perfection and served as a hearty main course.",
     img: "https://img.taste.com.au/8jgqEgm_/w720-h480-cfill-q80/taste/2016/11/classic-moussaka-79675-1.jpeg",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 24,
@@ -192,6 +236,8 @@ export const recipes = [
       "Pho Bo, also known as Vietnamese Beef Noodle Soup, is a popular Vietnamese dish consisting of beef broth, rice noodles, thinly sliced beef, and various herbs and toppings. It is a comforting and flavorful soup enjoyed throughout Vietnam.",
     img: "https://images.pexels.com/photos/6646072/pexels-photo-6646072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 25,
@@ -200,6 +246,8 @@ export const recipes = [
       "Salmon Teriyaki is a Japanese dish featuring grilled or pan-seared salmon glazed with a sweet and savory teriyaki sauce. It is often served with steamed rice and vegetables.",
     img: "https://images.pexels.com/photos/17402677/pexels-photo-17402677/free-photo-of-a-homemade-salmon-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 26,
@@ -208,6 +256,8 @@ export const recipes = [
       "Tandoori Chicken is a popular Indian dish made by marinating chicken in a spiced yogurt mixture and then cooking it in a tandoor (clay oven) or grill. The result is juicy and flavorful chicken with a vibrant red color.",
     img: "https://images.pexels.com/photos/9609832/pexels-photo-9609832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
   {
     id: 27,
@@ -216,6 +266,8 @@ export const recipes = [
       "Beef Stir-Fry is a quick and delicious dish made by stir-frying thinly sliced beef with vegetables, such as bell peppers, broccoli, and carrots, in a flavorful sauce. It is typically served with steamed rice or noodles.",
     img: "https://www.tablefortwoblog.com/wp-content/uploads/2022/03/beef-stir-fry-recipe-photo-tablefortwoblog-4-scaled.jpg",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Easy",
   },
   {
     id: 28,
@@ -224,6 +276,8 @@ export const recipes = [
       "Ratatouille is a traditional French vegetable stew made with a combination of eggplant, zucchini, bell peppers, tomatoes, onions, and herbs. It is a vibrant and flavorful dish that can be served as a main course or side dish.",
     img: "https://images.pexels.com/photos/7439978/pexels-photo-7439978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Europe",
+    difficulty: "Medium",
   },
   {
     id: 29,
@@ -232,6 +286,8 @@ export const recipes = [
       "Crispy Fried Chicken is a beloved dish made by coating chicken pieces with a seasoned flour or batter and deep-frying them until golden and crispy. It is often served with mashed potatoes, coleslaw, and a variety of dipping sauces.",
     img: "https://images.pexels.com/photos/6941021/pexels-photo-6941021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "America",
+    difficulty: "Easy",
   },
   {
     id: 30,
@@ -240,6 +296,8 @@ export const recipes = [
       "Ramen is a Japanese noodle soup dish with Chinese origins. It features wheat noodles served in a flavorful broth, topped with various ingredients such as sliced pork, soft-boiled egg, green onions, and seaweed.",
     img: "https://images.pexels.com/photos/2664216/pexels-photo-2664216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     link: "/Recipes",
+    category: "Asia",
+    difficulty: "Medium",
   },
 ];
 
