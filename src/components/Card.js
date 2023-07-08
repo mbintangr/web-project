a66974af3bda07a59adff1485fb42c381882efd2
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-08 15:53:00 +0700

    add hover shadow

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 function Card(props) {
   return (
-    <div className='flex-col my-3 mx-auto rounded-2xl shadow-xl bg-white min-w-[30%]  hover:scale-105 duration-300'>
-        <img src={props.src} alt={props.alt} className='rounded-t-xl shadow-lg w-[100%] h-[150px] object-cover lg:h-[250px]'/>
+      <div className='flex-col mx-auto rounded-2xl shadow-2xl hover:shadow-secondary/30 bg-white min-w-[30%] hover:scale-105 duration-300'>
+        <Link to={props.link}>
+          <img src={props.src} alt={props.alt} className='rounded-t-xl shadow-lg w-[100%] h-[200px] object-cover'/>
 
-        <div className='p-7'>
-          <h1 className='text-xl font-bold mt-2'>{props.title}</h1>
-          <p className='my-2'>{props.description}</p>
-          <Button link={props.link} title="Learn More" />
-        </div>
-    </div>
+          <div className='p-7'>
+            <h1 className='text-xl font-bold mt-2'>{props.title}</h1>
+            <p className='my-2'>{props.description}</p>
+            <Button link={props.link} title="Read More" />
+          </div>
+        </Link>
+      </div>
   )
 }
 
