02a219d98fef103fdc5a8d0ebada37eb90d81179
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-09 11:23:59 +0700

    change category card overlay opacity

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -4,7 +4,7 @@ const CategoryCard = ({ title, img }) => {
   return (
     <div className="sm:h-[100px] h-[75px] cursor-pointer">
       <div className="relative shadow-xl hover:shadow-secondary/30 rounded-xl hover:scale-105 duration-300">
-        <div className="absolute w-full sm:h-[100px] h-[75px] bg-black/50 rounded-xl text-tertiary flex justify-center items-center">
+        <div className="absolute w-full sm:h-[100px] h-[75px] bg-black/40 rounded-xl text-tertiary flex justify-center items-center">
           <h1 className="font-bold text-2xl">{title}</h1>
         </div>
         <img
