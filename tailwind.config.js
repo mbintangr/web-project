06d9837ea8cdb6087e98e7a929a99f37a5d6a73c
Bintang Ramadhan <mbintangr13@gmail.com>
2023-07-10 11:59:39 +0700

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,23 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    colors: {
+    /* colors: {
       'primary': '#4C4C6D',
       'secondary': '#1B9C85',
       'tertiary': '#E8F6EF',
       'quaternary': '#FFE194'
+    }, */
+    /* colors: {
+      'primary': '#D2001A',
+      'secondary': '#FFDE00',
+      'tertiary': '#FFFAE7',
+      'quaternary': '#EFEFEF'
+    }, */
+    colors: {
+      'primary': '#1A5D1A',
+      'secondary': '#F1C93B',
+      'tertiary': '#FBD85D',
+      'quaternary': '#FAE392'
     },
   },
   plugins: [require('flowbite/plugin')],
