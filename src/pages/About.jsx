09a0087f2a8837cfd1ce19cdc3c336c66caddcd6
DzakyN <mdzakynfl@gmail.com>
2023-07-11 12:14:55 +0700

    aa

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,46 +2,83 @@ import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import pict from '../assets/creator1.jpg';
 import picts from '../assets/asia.jpg';
+import Modal from 'react-modal';
 
 const About = () => {
-  const [showDescription, setShowDescription] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedCreator, setSelectedCreator] = useState(null);
 
-  const toggleDescription = () => {
-    setShowDescription(!showDescription);
+  const toggleModal = (creator) => {
+    setSelectedCreator(creator);
+    setShowModal(!showModal);
   };
 
   return (
     <Layout>
       <div className='flex flex-col items-center'>
         <h1 className='text-3xl font-bold pb-5'>About Us!</h1>
-        <div className='mb-auto grid grid-cols-2'>
-          <div className='flex flex-col items-center'>
+        <div className='mb-auto grid grid-cols-2 gap-4'>
+          <div
+            className='flex flex-col items-center cursor-pointer'
+            onClick={() => toggleModal('creator1')}
+          >
             <img
-              className='w-48 h-48 rounded-full cursor-pointer'
+              className='w-48 h-48 rounded-full'
               src={pict}
               alt='First Creator'
-              onClick={toggleDescription}
             />
-            {showDescription && (
-              <p className='mt-2 text-center'>
-                Description of the first creator goes here.
-              </p>
-            )}
           </div>
-          <div className='flex flex-col items-center mt-8'>
+          <div
+            className='flex flex-col items-center cursor-pointer'
+            onClick={() => toggleModal('creator2')}
+          >
             <img
-              className='w-48 h-48 rounded-full cursor-pointer'
+              className='w-48 h-48 rounded-full'
               src={picts}
               alt='Second Creator'
-              onClick={toggleDescription}
             />
-            {showDescription && (
-              <p className='mt-2 text-center'>
-                Description of the second creator goes here.
-              </p>
-            )}
           </div>
         </div>
+        <Modal
+          isOpen={showModal}
+          onRequestClose={toggleModal}
+          className='modal'
+          overlayClassName='overlay'
+        >
+          <div className='modal-content'>
+            {selectedCreator === 'creator1' && (
+              <div>
+                <img
+                  className='w-64 h-64 rounded-full mb-4'
+                  src={pict}
+                  alt='First Creator'
+                />
+                <p className='text-center'>
+                  Description of the first creator goes here.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Sed consequat arcu vitae enim efficitur, at dignissim purus ultrices.
+                </p>
+              </div>
+            )}
+            {selectedCreator === 'creator2' && (
+              <div>
+                <img
+                  className='w-64 h-64 rounded-full mb-4'
+                  src={picts}
+                  alt='Second Creator'
+                />
+                <p className='text-center'>
+                  Description of the second creator goes here.
+                  Vestibulum at nibh nec elit congue tincidunt.
+                  Aenean eget neque consequat, luctus felis ac, suscipit nunc.
+                </p>
+              </div>
+            )}
+            <button className='modal-close' onClick={toggleModal}>
+              Close
+            </button>
+          </div>
+        </Modal>
       </div>
     </Layout>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 function Contact() {
+  const message = 'Halo, saya ingin menghubungi Anda.';
+  const phoneNumber = '=+62 81289473692';
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
   return (
     <Layout>
       <div className="flex flex-col items-center">
         <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
         <p className="text-center mb-8">
-         Hubungi kami!
+         Halo saya tertarik untuk menghubungi anda
         </p>
         <a
-          href="https://api.whatsapp.com/send?phone=+62 81318041130"
+          href={whatsappLink}
           target="_blank"
           rel="noopener noreferrer"
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
