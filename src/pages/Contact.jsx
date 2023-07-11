e0d0136bb12857cafecf49dcf468850c84ecc780
DzakyN <mdzakynfl@gmail.com>
2023-07-11 15:03:42 +0700

    Menambahkan logo instagram

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import logo from '../assets/logo.png';
+import { FiPhone, FiInstagram, FiLinkedin } from 'react-icons/fi';
 
-function Contact() {
-  const message = 'Halo, saya ingin menghubungi Anda.';
-  const phoneNumber = '=+62 81289473692';
-  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+export default function Contact() {
+  const handleWhatsAppClick = () => {
+    window.open('https://wa.me/+62 857-1577-1891', '_blank');
+  };
+
+  const handleInstagramClick = () => {
+    window.open('https://www.instagram.com/cristiano', '_blank');
+  };
+
+  const handleLinkedInClick = () => {
+    window.open('https://www.linkedin.com/in/sandikagalih', '_blank');
+  };
 
   return (
     <Layout>
       <div className="flex flex-col items-center">
-        <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-        <p className="text-center mb-8">
-         Halo saya tertarik untuk menghubungi anda
-        </p>
-        <a
-          href={whatsappLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Contact via WhatsApp
-        </a>
+        <div className="my-8">
+          <img src={logo} alt="Logo" className="w-24 h-24 mb-4" />
+          <h1 className="text-3xl font-bold">Contact</h1>
+        </div>
+        <div className="my-4">
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold">Person 1</h2>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-110"
+              onClick={handleWhatsAppClick}
+            >
+              <FiPhone className="inline-block mr-2" />
+              Hubungi via WhatsApp
+            </button>
+            <button
+              className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-110"
+              onClick={handleInstagramClick}
+            >
+              <FiInstagram className="inline-block mr-2" />
+              Kunjungi Instagram
+            </button>
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-110"
+              onClick={handleLinkedInClick}
+            >
+              <FiLinkedin className="inline-block mr-2" />
+              Kunjungi LinkedIn
+            </button>
+          </div>
+        </div>
       </div>
     </Layout>
   );
 }
-
-export default Contact;
