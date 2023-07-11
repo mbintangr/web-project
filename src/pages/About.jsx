add3a04e8da978bdae9bc1e451a781272bb4d3cc
DzakyN <mdzakynfl@gmail.com>
2023-07-11 09:38:31 +0700

    Merubah pesan about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,23 @@ const About = () => {
     <Layout>
       <div className='mb-auto'>
         <h1 className='text-3xl font-bold pb-5 '>About Us!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi in nemo ut fuga, magnam libero ullam dolor atque temporibus nihil ratione impedit, accusantium provident, iure aliquam aperiam mollitia? Cupiditate accusantium esse corporis voluptatibus, dolores laboriosam illo vitae rem quaerat facilis earum consequatur perferendis error praesentium porro. Neque odio iste est corporis ipsam deserunt porro sunt at. Consequatur rerum perspiciatis qui praesentium quo ab iure fugiat asperiores distinctio laudantium, blanditiis minima animi velit ea harum, provident delectus. Eos ipsa, consequuntur culpa inventore saepe repudiandae ad ullam nemo amet quibusdam architecto repellendus natus nam quam blanditiis autem nostrum iste hic ab assumenda.</p>
+        <p>Halo semuanya!
+
+Kami senang sekali ingin memperkenalkan kepada kalian sebuah website yang penuh dengan cinta terhadap makanan dan teknologi. Izinkan kami memperkenalkan diri, kami adalah dua orang dengan hasrat yang sama dalam dunia kuliner dan ngoding. Dengan semangat dan keahlian kami yang saling melengkapi, kami menciptakan sebuah platform yang menyajikan beragam resep makanan, baik dari negeri maupun dari luar negeri.
+
+Kami percaya bahwa makanan adalah jendela untuk mengenal budaya, tradisi, dan keragaman dunia. Melalui makanan, kita dapat memperluas wawasan kita dan mengalami sensasi baru yang menggugah selera. Tidak hanya itu, kami juga memiliki cinta yang sama terhadap teknologi dan dunia pemrograman. Kami berdua menikmati proses menciptakan sesuatu dari nol, merangkai kode-kode yang menghasilkan sebuah website yang menarik dan interaktif.
+
+Website kami berfungsi sebagai pusat inspirasi bagi semua pencinta kuliner, baik mereka yang ingin mencoba masakan tradisional dari negeri ini maupun mereka yang penasaran dengan cita rasa dari berbagai penjuru dunia. Di sini, Anda akan menemukan berbagai macam resep yang terperinci, mulai dari hidangan khas Indonesia yang lezat hingga makanan eksotis dari benua lain.
+
+Kami berdua selalu bersemangat untuk mencoba resep baru, dan kami ingin berbagi pengetahuan dan pengalaman kami dengan Anda semua. Setiap resep yang kami bagikan telah melalui proses uji coba dan kami berusaha untuk memberikan petunjuk yang jelas dan mudah diikuti, sehingga siapa pun dapat mencoba memasaknya dengan percaya diri.
+
+Selain resep-resep yang luar biasa, kami juga ingin memberikan Anda pengalaman online yang menyenangkan. Dengan desain website yang menarik dan antarmuka yang mudah digunakan, kami berharap Anda dapat menemukan apa yang Anda cari dengan mudah. Kami juga memberikan fitur untuk menyimpan resep favorit Anda, berbagi komentar, dan bahkan mengunggah kreasi kuliner Anda sendiri.
+
+Kami berharap bahwa website kami dapat menginspirasi dan membangkitkan hasrat kalian dalam memasak dan ngoding. Jadi, tunggu apa lagi? Silakan kunjungi website kami dan jelajahi dunia kuliner yang menakjubkan dari seluruh penjuru dunia. Bersiaplah untuk petualangan baru dalam dunia makanan dan teknologi!
+
+Terima kasih atas dukungan dan kunjungan kalian.
+
+Salam, </p>
       </div>
     </Layout>
   )  
