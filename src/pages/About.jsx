c059f62f9e59b25d16a2c2de316e03f65074f4bd
DzakyN <mdzakynfl@gmail.com>
2023-07-11 11:30:15 +0700

    nambah gambar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,23 +6,7 @@ const About = () => {
     <Layout>
       <div className='mb-auto'>
         <h1 className='text-3xl font-bold pb-5 '>About Us!</h1>
-        <p>Halo semuanya!
-
-Kami senang sekali ingin memperkenalkan kepada kalian sebuah website yang penuh dengan cinta terhadap makanan dan teknologi. Izinkan kami memperkenalkan diri, kami adalah dua orang dengan hasrat yang sama dalam dunia kuliner dan ngoding. Dengan semangat dan keahlian kami yang saling melengkapi, kami menciptakan sebuah platform yang menyajikan beragam resep makanan, baik dari negeri maupun dari luar negeri.
-
-Kami percaya bahwa makanan adalah jendela untuk mengenal budaya, tradisi, dan keragaman dunia. Melalui makanan, kita dapat memperluas wawasan kita dan mengalami sensasi baru yang menggugah selera. Tidak hanya itu, kami juga memiliki cinta yang sama terhadap teknologi dan dunia pemrograman. Kami berdua menikmati proses menciptakan sesuatu dari nol, merangkai kode-kode yang menghasilkan sebuah website yang menarik dan interaktif.
-
-Website kami berfungsi sebagai pusat inspirasi bagi semua pencinta kuliner, baik mereka yang ingin mencoba masakan tradisional dari negeri ini maupun mereka yang penasaran dengan cita rasa dari berbagai penjuru dunia. Di sini, Anda akan menemukan berbagai macam resep yang terperinci, mulai dari hidangan khas Indonesia yang lezat hingga makanan eksotis dari benua lain.
-
-Kami berdua selalu bersemangat untuk mencoba resep baru, dan kami ingin berbagi pengetahuan dan pengalaman kami dengan Anda semua. Setiap resep yang kami bagikan telah melalui proses uji coba dan kami berusaha untuk memberikan petunjuk yang jelas dan mudah diikuti, sehingga siapa pun dapat mencoba memasaknya dengan percaya diri.
-
-Selain resep-resep yang luar biasa, kami juga ingin memberikan Anda pengalaman online yang menyenangkan. Dengan desain website yang menarik dan antarmuka yang mudah digunakan, kami berharap Anda dapat menemukan apa yang Anda cari dengan mudah. Kami juga memberikan fitur untuk menyimpan resep favorit Anda, berbagi komentar, dan bahkan mengunggah kreasi kuliner Anda sendiri.
-
-Kami berharap bahwa website kami dapat menginspirasi dan membangkitkan hasrat kalian dalam memasak dan ngoding. Jadi, tunggu apa lagi? Silakan kunjungi website kami dan jelajahi dunia kuliner yang menakjubkan dari seluruh penjuru dunia. Bersiaplah untuk petualangan baru dalam dunia makanan dan teknologi!
-
-Terima kasih atas dukungan dan kunjungan kalian.
-
-Salam, </p>
+        <p> Halkdsbfikuwefvujwevik</p>
       </div>
     </Layout>
   )  
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
 
 function Contact() {
   return (
     <Layout>
-      <div>Contact</div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
+        <p className="text-center mb-8">
+         Hubungi kami!
+        </p>
+        <a
+          href="https://api.whatsapp.com/send?phone=YOUR_PHONE_NUMBER"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Contact via WhatsApp
+        </a>
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
